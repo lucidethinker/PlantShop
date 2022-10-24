7aeca151d1452813f4868300a8720256728e9671
jasim <jasimihsan2@gmail.com>
2022-10-24 16:37:56 +0530

    footer design partially done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -42,10 +42,57 @@ const Footer = () => {
               </ListGroupItem>
               </ListGroup>
           </div>
-
         </Col>
-        <Col lg='2'></Col>
-        <Col lg='1'></Col>
+        <Col lg='2'>
+        <div className="footer__quick-links">
+            <h4 className="quick__link-title">Useful Links</h4>
+            <ListGroup className="mb-3">
+              <ListGroupItem className="ps-0 border-0">
+                <Link to='/shop'>
+                Shop
+                </Link>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0">
+                <Link to='/cart'>
+                Cart
+                </Link>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0">
+                <Link to='/login'>
+                Login
+                </Link>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0">
+                <Link to='#'>
+                Privacy Policy
+                </Link>
+              </ListGroupItem>
+              </ListGroup>
+          </div>
+        </Col>
+        <Col lg='1'>
+        <div className="footer__quick-links">
+            <h4 className="quick__link-title">Contact</h4>
+            <ListGroup className="mb-3">
+              <ListGroupItem className="ps-0 border-0">
+              <span><i class="ri-map-pin-line"></i></span>
+              <p>676303 Parappanagadi ullanam north po india</p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0">
+              <span><i class="ri-phone-line"></i></span>
+              <p>+919400373764</p>
+              </ListGroupItem>
+
+              <ListGroupItem className="ps-0 border-0">
+              <span><i class="ri-mail-line"></i></span>
+              <p>jasimihsan@outlook.com</p>
+              </ListGroupItem>
+              </ListGroup>
+
+
+          </div>
+        </Col>
+
       </Row>
     </Container>
   </footer>
