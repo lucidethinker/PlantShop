cf7c11a90c8dbcc7f513da16f60b63d5e7059ae9
jasim <jasimihsan2@gmail.com>
2022-10-24 12:33:07 +0530

    Home page design is done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+
+import {Link} from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+
 import Helmet from '../components/Helmet/Helmet'
-import heroImg from'../assets/images/hero-img.png'
 import '../styles/home.css'
 
 import{ Container, Row, Col } from 'reactstrap'
+import heroImg from'../assets/images/hero-img.png'
 
+import Services from '../services/Services';
 const Home = () => {
   const year = new Date().getFullYear()
   return <Helmet title={'Home'}>
@@ -19,7 +25,10 @@ const Home = () => {
             <h2>Mollit voluptate magna ut Lorem voluptate non veniam.</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae deleniti, dolorem ipsam doloribus accusamus recusandae minus totam veritatis autem repellendus!
             </p>
-            <button className="buy__btn">SHOP NOW</button>
+
+            <motion.button whileTap={{scale: 0.9}} className="buy__btn">
+              <Link to='/shop'>Shop Now</Link></motion.button>
+
           </div>
           </Col>
           <Col lg='6' md='6'>
@@ -32,6 +41,7 @@ const Home = () => {
           </Row>
           </Container>
     </section>
+    <Services />
   </Helmet>
 };
 
