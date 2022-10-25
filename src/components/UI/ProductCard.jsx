d0c47e759d09942c333ebbdb1894a01675aea2c1
jasim <jasimihsan2@gmail.com>
2022-10-26 04:51:38 +0530

    initial commit

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -4,7 +4,24 @@ import '../../styles/product-card.css'
 import {Col} from 'reactstrap'
 import {Link} from 'react-router-dom'
 
+import { useDispatch } from 'react-redux'
+import{cartActions} from '../../Redux/slices/cartSlice'
+
 const ProductCard = ({item}) => {
+
+    const dispatch = useDispatch()
+
+    const addToCart = () => {
+        dispatch(cartActions.addItem({
+            id:item.id,
+            productName:item.productName,
+            price:item.price,
+            image:item.imageUrl
+
+        }))
+        alert('product added to cart')
+    }
+
   return (
    <Col lg='3' md='4' className="mb-2">
     <div className="product__item">
@@ -18,7 +35,7 @@ const ProductCard = ({item}) => {
         <span >{item.category}</span></div>
         <div className="product__card-bottom d-Flex align-items-center justify-content-between p-2">
             <span className="price">₹{item.price}</span>
-            <motion.span whileTap={{scale:1.2}}>
+            <motion.span whileTap={{scale:1.2}} onClick={addToCart}>
                 <i class="ri-add-box-line"></i>
                 </motion.span>
         </div>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+import store from "./Redux/store";
+import {Provider} from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
    <App/>
+   </Provider>
    </BrowserRouter>
   </React.StrictMode>
 );
