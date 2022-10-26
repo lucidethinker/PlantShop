5c5e96059ddef5e82c490f7442aa66c4067e3a17
jasim <jasimihsan2@gmail.com>
2022-10-26 06:58:49 +0530

    Shop initial commit 👊🏿👊🏿

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import  CommonSection  from '../components/UI/CommonSection'
+import Helmet from '../components/Helmet/Helmet'
+import {Container,Row,Col} from 'reactstrap'
+
 const Shop = () => {
-  return (
-    <div>Shop</div>
-  )
+  return <Helmet title="Shop">
+    <CommonSection title="Products"/>
+    <section>
+    <Container>
+      <Row>
+        <Col lg='3' md='3'>
+            <div className="filter__widget">
+              <select  >
+                <option value="Indor Plant">Indor Plant</option>
+                <option value="Outdor Plant">Outdor Plant"</option>
+                <option value="Seed">Seed</option>
+              </select>
+            </div>
+
+        </Col>
+        <Col lg='3' md='3'></Col>
+        <Col lg='3' md='3'></Col>
+      </Row>
+      </Container>
+    </section>
+  </Helmet>
 }
 
 export default Shop
