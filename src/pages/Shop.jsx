d63107b710a79cafbfa1c98a99227fee710afbeb
jasim <jasimihsan2@gmail.com>
2022-10-26 07:06:35 +0530

    Shop initial commit 👊🏿👊🏿

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -13,6 +13,7 @@ const Shop = () => {
         <Col lg='3' md='3'>
             <div className="filter__widget">
               <select  >
+                <option > Filter By Category</option>
                 <option value="Indor Plant">Indor Plant</option>
                 <option value="Outdor Plant">Outdor Plant"</option>
                 <option value="Seed">Seed</option>
@@ -21,7 +22,20 @@ const Shop = () => {
 
         </Col>
         <Col lg='3' md='3'></Col>
-        <Col lg='3' md='3'></Col>
+        <div className="filter__widget">
+              <select  >
+                <option > Sort By </option>
+                <option value="Ascending">Ascending</option>
+                <option value="Descending">Descending</option>
+                <option value="Seed">Seed</option>
+              </select>
+            </div>
+        <Col lg='6' md='6'>
+            <div className="search__box">
+                  <input type="text" placeholder="Search....." />
+            </div>
+
+        </Col>
       </Row>
       </Container>
     </section>
