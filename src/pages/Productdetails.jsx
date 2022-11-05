e3609955784a7dacefe57c067341679b8e292b94
jasim <jasimihsan2@gmail.com>
2022-11-05 11:14:10 +0530

    Product page second commit  .. added review and product details

--- src/pages/Productdetails.jsx
+++ src/pages/Productdetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState ,useRef} from 'react'
 
 import { Container, Row, Col} from 'reactstrap'
 import { useParams } from 'react-router-dom'
@@ -6,48 +6,167 @@ import products from '../assets/data/products'
 import Helmet from '../components/Helmet/Helmet'
 import CommonSection from '../components/UI/CommonSection'
 import '../styles/product-details.css'
+import {motion} from 'framer-motion'
+import ProductList from '../components/UI/ProductList'
+import{ useDispatch } from 'react-redux'
+import {cartActions} from '../Redux/slices/cartSlice'
+import {toast} from "react-toastify"
+
 
 const Productdetails = () => {
+ 
 
+  const [tab,setTab] = useState('desc')
+  const reviewUser = useRef("")
+  const reviewMsg = useRef("")
+  const dispatch = useDispatch()
+
+  const[rating,setRating] = useState(null)
   const {id } = useParams()
   const product = products.find(item=>item.id === id)
 
-  const {imgUrl,productName, price,avgRating,review,description,shortDesc} = 
-  product
+  const {imgUrl,productName, price,avgRating,reviews,description,shortDesc,category} 
+  = product;
 
 
+const relatedProducts = products.filter(item => item.category===category)
+
+const submitHandler = (e) => {
+  
+  e.preventDefault()
+  const reviewUsername = reviewUser.current.value
+  const reviewUserMsg = reviewMsg.current.value
+
+};
+
+const addToCart =() => {
+  dispatch(cartActions.addItem({
+    id,
+    image:imgUrl,
+    productName,
+    price,
 
 
-  return <Helmet>
-    <CommonSection/>
+  }))
+
+   toast.success("Item added to cart")
+
+}
+
+
+  return <Helmet title={productName}>
+    <CommonSection title={productName}/>
     <section  className="pt-0">
       <Container>
         <Row>
           <Col lg='6'>
             <img src={imgUrl} alt=''/></Col>
           <Col lg='6'>
-                  <div className="product__details d-flex ali">
+                  <div className="product__details d-flex all">
                     <div >
                     <h1>{productName}</h1>
                     <div className="product__rating d-flex align-items-center gap-5 mb-3"></div>
                   <div>
-                  <span> <i class="ri-star-fill"></i></span>
-                  <span> <i class="ri-star-fill"></i></span>
-                  <span> <i class="ri-star-fill"></i></span>
-                  <span> <i class="ri-star-fill"></i></span>
-                  <span> <i class="ri-star-half-line"></i></span>
+                  <span onClick={() =>setRating(1)}> <i class="ri-star-fill"></i></span>
+                  <span onClick={() =>setRating(2)}> <i class="ri-star-fill"></i></span>
+                  <span onClick={() =>setRating(3)}> <i class="ri-star-fill"></i></span>
+                  <span onClick={() =>setRating(4)}> <i class="ri-star-fill"></i></span>
+                  <span onClick={() =>setRating(5)}> <i class="ri-star-fill"></i></span>
+                  
           </div>
           <p>(<span >{avgRating}</span>ratings)
           </p>
           </div>
            </div>
-          <span className="product__price">{price}</span>
+           <div className="d-flex align-items-center gap-5">
+          <span className="product__price">₹{price}</span>
+          <span>
+            Category:{category.toLowerCase()}
+          </span>
+          </div>
           <p>{shortDesc}</p>
 
-          <button className="buy__btn"> Add to Cart</button>
+          <motion.button 
+          whileTap={{scale:1.2}} 
+          className="buy__btn" onClick={addToCart}> Add to Cart</motion.button>
           </Col>
           </Row>
       </Container>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              <div className="tab__wraper d-flex align-items-center gap-5">
+                <h6 className={`${tab ==="desc" ? "active__tab" :""}`} onClick={()=>setTab('desc')}>Description</h6>
+                <h6 className={`${tab ==="rev" ? "active__tab" :""}`} onClick={()=>setTab('rev')} >Reviews ({reviews.length})</h6>
+              </div>
+
+
+
+              {
+                tab === "desc" ?<div className="tab__content mt-5">
+                <p>{description}</p>
+              </div> : <div className='product__review mt-5'>
+                <div className="review__wraper ">
+                  <ul>
+                    {
+                      reviews.map((item,index)=>(
+                        <li  key={index} className="mb-4">
+                        <h6>Appu </h6>  
+                        <span>{item.rating} (rating)</span>
+                        <p>{item.text}</p>
+                        </li>
+
+                      ))
+                    }
+                  </ul>
+
+                  <div className="review__form">
+                    <h4> Leave Your Experience</h4>
+
+                    <form action="" onSubmit={submitHandler}>
+                      <div className="form__group">
+                        <input type="text" placeholder="Enter name"
+                        ref={reviewUser}/>
+                      </div>
+
+                     <div className="form__group d-flex align-items-center gap-5">
+                      <span>1 <i class="ri-star-fill"></i></span>
+                      <span>2 <i class="ri-star-fill"></i></span>
+                      <span>3 <i class="ri-star-fill"></i></span>
+                      <span>4 <i class="ri-star-fill"></i></span>
+                      <span>5 <i class="ri-star-fill"></i></span>
+                     </div>
+
+                     <div className="form__group ">
+                        <textarea 
+                        ref = {reviewMsg}
+                        rows={4}
+                         type="text" 
+                         placeholder="Review Message ...."/>
+                      </div>
+                      <button type='submit' className="buy__btn">
+                        Submit
+                      </button>
+                    </form>
+
+                  </div>
+                </div>
+              </div>
+              }
+
+            
+            </Col>
+
+            <Col lg='12' className='mt-5'>
+              <h2 className="related__title">
+                You might also like
+              </h2>
+            </Col>
+            <ProductList data={relatedProducts} />
+          </Row>
+        </Container>
+      </section>
   
     </section>
   </Helmet>
