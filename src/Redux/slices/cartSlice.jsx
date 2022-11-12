954b1af26d8885daa00bb5eb236bbcae218833c4
jasim <jasimihsan2@gmail.com>
2022-11-12 12:03:02 +0530

    cart page is partially done

--- src/Redux/slices/cartSlice.jsx
+++ src/Redux/slices/cartSlice.jsx
@@ -40,6 +40,18 @@ const cartSlice = createSlice({
 
 
 },
+deleteItem:(state,action)=>{
+  const id = action.payload;
+  const existingItem = state.cartItems.find(item=>item.id === id);
+
+  if(existingItem){
+    state.cartItems = state.cartItems.filter(item=>item.id !== id)
+    state.totalQuantity = state.totalQuantity - existingItem.quantity;
+  }
+  
+  state.totalAmount = state.cartItems.reduce((total,item)=>total+Number(item.price)*Number(item.quantity))
+
+},
   },
 });
 
