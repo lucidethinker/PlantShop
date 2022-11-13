7fba12ae4ffe0e8d75f716d3069998222e47d5ec
jasim <jasimihsan2@gmail.com>
2022-11-13 16:48:05 +0530

    authentication first commit ..🔒🗝️

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,11 @@
 import React,{useState} from 'react'
 import Helmet from '../components/Helmet/Helmet';
 import { Container,Row,Col,Form,FormGroup } from 'reactstrap';
-import{Link} from 'react-router-dom'
+import{Link,useNavigate} from 'react-router-dom'
+import {signInWithEmailAndPassword } from 'firebase/auth'
+import {auth} from '../firebase.config'
+import {toast} from 'react-toastify'
+
 
 import '../styles/login.css'
 
@@ -11,27 +15,53 @@ import '../styles/login.css'
 const Login = () => {
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
 
+  const signIn = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try{
+           const userCredential = await signInWithEmailAndPassword(auth,email,password);
+    
+           const user = userCredential.user;
+           console.log(user)
+           setLoading(false);
+           toast.success('Login Successfull')
+           navigate('/checkout')
+
+          }catch(error){
+      setLoading(false);
+      toast.error(error.message)
+    }
+  }
   return <Helmet title="Login">
     <section>
       <Container>
       <Row>
-        <Col lg='6' className='m-auto text-center'>
-          <h3 className='fw-bold mb-4 '> Login</h3>
-          <Form className='auth__form'>
-            <FormGroup className='form__group'>
-                <input type="email" placeholder="Enter your email address"  value={email} onChange={e=>setEmail(e.target.value)} />
-                </FormGroup>
-                <FormGroup className='form__group'>
-                <input type="password" placeholder="Enter your password" value={password} onChange={e=>setPassword(e.target.value)} />
-                </FormGroup>
-
-                <button type="submit" className="buy__btn auth__btn">Login</button>
-                <p>Don't have an account yet?{" "} <Link to="/signup
-                ">Create an account</Link></p>
-          </Form>
-        
-        </Col>
+        {
+          loading ? <Col lg='12'
+           className='text-center'>
+            <h5 className='fw-bold'>Loading .... </h5>
+            </Col>:
+            <Col lg='6' className='m-auto text-center'>
+            <h3 className='fw-bold mb-4 '> Login</h3>
+            <Form className='auth__form' onSubmit={signIn}>
+              <FormGroup className='form__group'>
+                  <input type="email" placeholder="Enter your email address"  value={email} onChange={e=>setEmail(e.target.value)} />
+                  </FormGroup>
+                  <FormGroup className='form__group'>
+                  <input type="password" placeholder="Enter your password" value={password} onChange={e=>setPassword(e.target.value)} />
+                  </FormGroup>
+  
+                  <button type="submit" className="buy__btn auth__btn">Login</button>
+                  <p>Don't have an account yet?{" "} <Link to="/signup
+                  ">Create an account</Link></p>
+            </Form>
+          
+          </Col>
+        }
       </Row>
       </Container>
 
