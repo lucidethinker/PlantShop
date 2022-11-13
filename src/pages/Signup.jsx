b51785c62fc822a3939aa22d13da73db656021f0
jasim <jasimihsan2@gmail.com>
2022-11-13 12:26:24 +0530

    sigup page initial

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,31 +4,43 @@ import Helmet from '../components/Helmet/Helmet';
 import { Container,Row,Col,Form,FormGroup } from 'reactstrap';
 import{Link} from 'react-router-dom'
 
-import '../styles/signup.css'
+import '../styles/login.css'
 
 
 
 const Signup = () => {
+  const [username,setUsername] = useState('');
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
+  const[file,setFile]= useState(null);
 
-  return <Helmet title="Login">
+  return <Helmet title="Signup">
     <section>
       <Container>
       <Row>
         <Col lg='6' className='m-auto text-center'>
-          <h3 className='fw-bold mb-4 '> Login</h3>
+          <h3 className='fw-bold mb-4 '> Signup</h3>
           <Form className='auth__form'>
             <FormGroup className='form__group'>
-                <input type="email" placeholder="Enter your email address"  value={email} onChange={e=>setEmail(e.target.value)} />
+                <input type="email" placeholder="Username"  value={username} onChange={e=>setUsername(e.target.value)} />
                 </FormGroup>
+               
+                <FormGroup className='form__group'>
+                <input type="email" placeholder="Email"  value={email} onChange={e=>setEmail(e.target.value)} />
+                </FormGroup>
+
+                
                 <FormGroup className='form__group'>
                 <input type="password" placeholder="Enter your password" value={password} onChange={e=>setPassword(e.target.value)} />
                 </FormGroup>
 
-                <button type="submit" className="buy__btn auth__btn">Login</button>
-                <p>Don't have an account yet?{" "} <Link to="/signup
-                ">Create an account</Link></p>
+                <FormGroup className='form__group'>
+                <input type="file" onChange={e=>setFile(e.target.files[0])} />
+                </FormGroup>
+
+                <button type="submit" className="buy__btn auth__btn">Create an account</button>
+                <p>Already have an account?{" "} <Link to="/login
+                ">Login</Link></p>
           </Form>
         
         </Col>
