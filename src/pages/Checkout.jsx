4ad75f00cffa595e7e1f7572535a547646daeec4
jasim <jasimihsan2@gmail.com>
2022-11-13 12:11:48 +0530

    Login page first commit 👀👀

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,7 +3,15 @@ import {Container, Row, Col,Form,FormGroup} from 'reactstrap'
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection  from '../components/UI/CommonSection'
 import '../styles/checkout.css';
+
+
+import { useSelector } from 'react-redux';
+
 const Checkout = () => {
+
+  const totalQty = useSelector(state => state.cart.totalQuantity);
+  const totalAmount = useSelector(state => state.cart.totalAmount);
+
   return <Helmet title="Checkout">
       <CommonSection title="Checkout"/>
       <section>
@@ -42,13 +50,22 @@ const Checkout = () => {
            </Col>
            <Col lg="4"> 
            <div className="checkout__cart">
-            <h6>Total Qty:<span>0</span></h6>
-            <h6>Subtotal:<span>₹100</span></h6>
-            <h6>Subtotal:<span>₹100</span></h6>
-            <h6>Free shipping </h6>
-            <h4>Total Cost : <span>₹120</span> </h4>
+            <h6>Total Qty:<span>{totalQty} items</span></h6>
+            <h6>Subtotal:<span>₹{totalAmount}</span></h6>
+            <h6>
+              <span>
+      
+              Shipping:<br />
+              free shipping
+              </span><span>₹100
+              </span> 
+              </h6>
+            <h4>Total Cost : <span>₹{totalAmount}</span> 
+            </h4>
            </div>
-           <button className="buy__btn auth__btn w-100">Place an order</button>
+           <button className="buy__btn auth__btn w-100">
+            Place an order
+           </button>
            </Col>
               </Row>
               </Container>
