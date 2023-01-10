339530a7e72d085f348c7dd659f709bda3072819
sanoofp <sanoofp7@gmail.com>
2023-01-10 11:45:24 +0530

    1

---
+++ src/assets/data/data.js
@@ -0,0 +1,348 @@
+import productImg01 from "../images/moneytree.jpg";
+import productImg02 from "../images/Fiddleleaffig.webp";
+import productImg03 from "../images/Rubberplant.webp";
+import productImg04 from "../images/Giant bird of paradise.jpg";
+import productImg05 from "../images/Dracaenamarginata.jpg";
+import productImg007 from "../images/geranium-pelargonium.webp";
+import productImg07 from "../images/purple-petunias.webp";
+import productImg13 from "../images/phone-01.jpg";
+
+export const products = [
+  {
+    id: "01",
+    productName: "Money Tree",
+    imgUrl: productImg01,
+    category: "Indor Plant",
+    price: 100,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.7,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.5,
+  },
+
+  {
+    id: "02",
+    productName: "Fiddle Leaf Fig",
+    imgUrl: productImg02,
+    category: "Indor Plant",
+    price: 1250,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+
+  {
+    id: "03",
+    productName: "Rubber plant",
+    imgUrl: productImg03,
+    category: "Indor Plant",
+    price: 173,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+  {
+    id: "26",
+    productName: "Giant bird of paradise",
+    imgUrl: productImg05,
+    category: "Indor Plant",
+    price: 253,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+  {
+    id: "04",
+    productName: "Giant bird of paradise",
+    imgUrl: productImg04,
+    category: "Indor Plant",
+    price: 1060,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+
+  {
+    id: "05",
+    productName: "Madagascar dragon tree",
+    imgUrl: productImg05,
+    category: "Indor Plant",
+    price: 163,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+
+  {
+    id: "06",
+    productName: "Dracaena corn plant",
+    imgUrl: productImg05,
+    category: "Indor Plant",
+    price: 163,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+  {
+    id: "07",
+    productName: "Geraniums",
+    imgUrl: productImg007,
+    category: "Outdor Plant",
+    price: 99,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+
+  {
+    id: "27",
+    productName: "Geraniums ",
+    imgUrl: productImg07,
+    category: "Outdor Plant",
+    price: 173,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+
+  {
+    id: "08",
+    productName: "Dolore dolore ",
+    imgUrl: productImg07,
+    category: "Outdor Plant",
+    price: 89,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+
+  {
+    id: "09",
+    productName: "Helmar Chair",
+    imgUrl: productImg07,
+    category: "Outdor Plant",
+    price: 112,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
+
+  {
+    id: "10",
+    productName: "Apple iPhone 12 Pro",
+    imgUrl: productImg13,
+    category: "",
+    price: 799,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.8,
+  },
+
+  {
+    id: "19",
+    productName: "Irure id enim ",
+    imgUrl: productImg05,
+    category: "seed",
+    price: 399,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.8,
+  },
+
+  {
+    id: "20",
+    productName: "Eiusmod quis nostrud ",
+    imgUrl: productImg01,
+    category: "seed",
+    price: 199,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.8,
+  },
+
+  {
+    id: "22",
+    productName: "Beat EP ",
+    imgUrl: productImg05,
+    category: "seed",
+    price: 199,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.8,
+  },
+];
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,116 +1,99 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-import  CommonSection  from '../components/UI/CommonSection'
-import Helmet from '../components/Helmet/Helmet'
-import {Container,Row,Col} from 'reactstrap'
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
 
+import "../styles/shop.css";
+import useStore from "../assets/data/products";
+import ProductList from "../components/UI/ProductList";
 
-import '../styles/shop.css'
-import useStore from '../assets/data/products'
-import ProductList from '../components/UI/ProductList'
+const FilterInput = () => {
+  const filter = useStore((state) => state.filter);
+  const setFilter = useStore((state) => state.setFilter);
 
-
-
-
-
-
-
-const Shop = () => {
-
-  const[productsData,setProductsData] = useState(products)
-
-  
-  const handleFilter = (e) =>{
-    const filterValue = e.target.value;
-    if(filterValue === 'Indor Plants')
-    {
-      const filteredProducts = products.filter((item)=>item.category === 'Indor Plant'
-      );
-      setProductsData(filteredProducts)
-  }
-  if(filterValue === 'Outdor Plants')
-    {
-      const filteredProducts = products.filter((item)=>item.category === 'Outdor Plant'
-      );
-      setProductsData(filteredProducts)
-  }
-  if(filterValue === 'Seeds')
-    {
-      const filteredProducts = products.filter((item)=>item.category === 'seed'
-      );
-      setProductsData(filteredProducts)
-  }
-  
-  
+  return (
+    <div>
+      <input
+        value={filter}
+        type="text"
+        placeholder="Search....."
+        onChange={(e) => setFilter(e.target.value)}
+      />
+    </div>
+  );
 };
 
-const FilterInput = () =>{
-  const items = useStore((state) => state.products);
-  const name = items.map((item) =>
-  item.productName
-  )
-  const setFilter = useStore((state) => state.setFilter);
-return(
-  <div>
-     <input  type="text" placeholder="Search....."   onChange={(e)=>setFilter(e.target.value)}/>
-  </div>
+const Shop = () => {
+  const [productsData, setProductsData] = useState(products);
 
-);}
+  const handleFilter = (e) => {
+    const filterValue = e.target.value;
+    if (filterValue === "Indor Plants") {
+      const filteredProducts = products.filter((item) => item.category === "Indor Plant");
+      setProductsData(filteredProducts);
+    }
+    if (filterValue === "Outdor Plants") {
+      const filteredProducts = products.filter((item) => item.category === "Outdor Plant");
+      setProductsData(filteredProducts);
+    }
+    if (filterValue === "Seeds") {
+      const filteredProducts = products.filter((item) => item.category === "seed");
+      setProductsData(filteredProducts);
+    }
+  };
 
-
-
-
-
-
-  return <Helmet title="Shop">
-    <CommonSection title="Products"/>
-    <section>
-    <Container>
-      <Row>
-        <Col lg='3' md='6'>
-            <div className="filter__widget">
-              <select onChange={handleFilter} >
-                <option > Filter By Category</option>
-                <option value="Indor Plants">Indoor Plants</option>
-                <option value="Outdor Plants">Outdor Plants</option>
-                <option value="Seeds">Seeds</option>
-              </select>
-            </div>
-        </Col>
-        <Col lg='3' md='6' className='text-end'>
-        <div className="filter__widget">
-              <select  >
-                <option > Sort By </option>
-                <option value="Ascending">Ascending</option>
-                <option value="Descending">Descending</option>
-                <option value="Seed">Seed</option>
-              </select>
-            </div>
+  return (
+    <Helmet title="Shop">
+      <CommonSection title="Products" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3" md="6">
+              <div className="filter__widget">
+                <select onChange={handleFilter}>
+                  <option> Filter By Category</option>
+                  <option value="Indor Plants">Indoor Plants</option>
+                  <option value="Outdor Plants">Outdor Plants</option>
+                  <option value="Seeds">Seeds</option>
+                </select>
+              </div>
             </Col>
-        <Col lg='6' md='12'>
-            <div className="search__box">
-                <FilterInput/>
-                  <span >
+            <Col lg="3" md="6" className="text-end">
+              <div className="filter__widget">
+                <select>
+                  <option> Sort By </option>
+                  <option value="Ascending">Ascending</option>
+                  <option value="Descending">Descending</option>
+                  <option value="Seed">Seed</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="6" md="12">
+              <div className="search__box">
+                <FilterInput />
+                <span>
                   <i class="ri-search-2-line"></i>
-                  </span>
-            </div>
-        </Col>
-      </Row>
-      </Container>
-    </section>
-
-    <selection className='pt-05'>
-      <Container>
-        <Row>
-          {
-            productsData.length === 0? <h1 className='text-center fs-4'>No products are found!</h1>
-            :<ProductList data={productsData} />  }
-        </Row>
+                </span>
+              </div>
+            </Col>
+          </Row>
         </Container>
-    </selection>
+      </section>
 
-      
-  </Helmet>
-}
+      <selection className="pt-05">
+        <Container>
+          <Row>
+            {productsData.length === 0 ? (
+              <h1 className="text-center fs-4">No products are found!</h1>
+            ) : (
+              <ProductList data={productsData} />
+            )}
+          </Row>
+        </Container>
+      </selection>
+    </Helmet>
+  );
+};
 
 export default Shop;
