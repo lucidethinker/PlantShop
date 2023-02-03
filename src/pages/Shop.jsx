d4cdc5207acf1e163cd20c4ad3f9dbd3208081fa
lucidethinkers <jasimihsan2@gmail.com>
2023-02-03 16:29:39 +0530

    search bar bug fix ✅  & catogory and sorting initial commit

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect,useCallback } from 'react';
 import CommonSection from "../components/UI/CommonSection";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
+import ProductList from '../components/UI/ProductList'
 
 import "../styles/shop.css";
 
@@ -27,56 +28,82 @@ const Shop = () =>
     });
 
   return (
-    <Helmet title="Shop">
-       <CommonSection title="Products"/>
-       <section>
-    <Container>
-      <Row>
-        <Col lg='3' md='6'>
-            <div className="filter__widget">
-            <select >
-                <option > Filter By Category</option>
-                <option value="Indor Plants">Indoor Plants</option>
-                <option value="Outdor Plants">Outdor Plants</option>
-                <option value="Seeds">Seeds</option>
-              </select>
-            </div>
-        </Col>
-        <Col lg='3' md='6' className='text-end'>
+<Helmet title="Shop">
+<CommonSection title="Products"/>
+<section>
+<Container>
+  <Row>
+    <Col lg='3' md='6'>
         <div className="filter__widget">
-              <select  >
-                <option > Sort By </option>
-                <option value="Ascending">Ascending</option>
-                <option value="Descending">Descending</option>
-                <option value="Seed">Seed</option>
-              </select>
-            </div>
-            </Col>
-            <Col lg='6' md='12'>
-    <div className="search__box">
-      <input
+          <select  >
+            <option > Filter By Category</option>
+            <option value="Indor Plants">Indoor Plants</option>
+            <option value="Outdor Plants">Outdor Plants</option>
+            <option value="Seeds">Seeds</option>
+          </select>
+        </div>
+    </Col>
+    <Col lg='3' md='6' className='text-end'>
+    <div className="filter__widget">
+          <select  >
+            <option > Sort By </option>
+            <option value="Ascending">Ascending</option>
+            <option value="Descending">Descending</option>
+            <option value="Seed">Seed</option>
+          </select>
+        </div>
+        </Col>
+    <Col lg='6' md='12'>
+        <div className="search__box">
+        <input
         type="text"
         placeholder="Search....."
         value={filter}
         onChange={(e) => setFilter(e.target.value)}
       />
-      <span >
-      <i class="ri-search-2-line"></i>
-</span></div>
-      {sortedProducts.map((product) => (
-        <div key={product.id}>
-          <h3>{product.productName}</h3>
-          <p>{product.category}</p>
-          <p> ₹ {product.price}</p>
-          <img src={product.imgUrl} alt={product.productName} />
+              <span >
+              <i class="ri-search-2-line"></i>
+              </span>
         </div>
-      ))} 
-        <p>No items found</p>
-        </Col>
-      </Row>
-      </Container>
-    </section>
-    </Helmet>
+    </Col>
+  </Row>
+  </Container>
+</section>
+
+<selection className='pt-05'>
+  <Container>
+    <Row>
+    {
+            sortedProducts.length === 0? <h1 className='text-center fs-4'>No products are found!</h1>
+            :<ProductList data={sortedProducts} />  }
+    </Row>
+    </Container>
+</selection>
+
+  
+</Helmet>
   );
 };
 export default Shop;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
